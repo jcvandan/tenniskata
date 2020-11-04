6028cc77f56f6b4d457aa11688956b26cf0a31c8
Dan Ormisher <dan.ormisher@open-money.co.uk>
2020-11-04 09:40:11 +0000

    Added sln & project

---
+++ TennisKata/TennisGame.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace TennisKata
+{
+    public class TennisGame
+    {
+        
+    }
+
+    public class TennisGameTests
+    {
+        [Fact]
+        public void Test()
+        {
+            
+        }
+    }
+}
